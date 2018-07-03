0c463f625d1d99d6d39e86e1db90c8b8450127f1
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-07-03 15:50:13 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -15,7 +15,7 @@ def fourth_challenge
       :heroine_friends => [
         #your hashes here!
         {name:"Steven" , age:"30" , attitude:"confused"},
-        {name: , age: , attitude:}
+        {name:"Nurse" , age:"44" , attitude:"worried"}
       ]
    }
 }
