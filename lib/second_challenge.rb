0c4ff6be8bc763ed85d81165ead658073d98b6c7
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-07-03 15:20:08 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,20 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch =>{},
+      :matriatch =>{},
+      :hero => {},
+      :hero_friends =>[]
       }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch =>{},
+      :matriatch =>{},
+      :heroine => {},
+      :heroine_frineds => []
+      }
+    
+  }
 
   
 end
