3d49802b5ae5f12b75634a87934064a06150a886
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-07-03 15:32:02 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,14 +3,14 @@ def second_challenge
     :montague => {
       #your key/value pairs here!
       :patriarch =>{},
-      :matriatch =>{},
+      :matriarch =>{},
       :hero => {},
       :hero_friends =>[]
       }, 
     :capulet => {
       #your key/value pairs here!
       :patriarch =>{},
-      :matriatch =>{},
+      :matriarch =>{},
       :heroine => {},
       :heroine_frineds => []
       }
